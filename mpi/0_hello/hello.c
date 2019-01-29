ab6aba439a310a4d939019d8e00d1027a30c3802
Junwei Zhang <zjunweihit@sina.com>
2019-01-29 13:07:15 +0800

    mpi: add function comments in hello

--- mpi/0_hello/hello.c
+++ mpi/0_hello/hello.c
@@ -1,6 +1,38 @@
 #include <mpi.h>
 #include <stdio.h>
 
+/*
+ * MPI_Init(
+ *     int* argc,
+ *     char*** argv)
+ *
+ *   global and internal variables are set up.
+ *     a communicator is created for all processes
+ *     unique ranks are assigned to each process.
+ *
+ * MPI_Comm_size(
+ *     MPI_Comm communicator,
+ *     int* size)
+ *
+ *   return the size of communicator
+ *   MPI_COMM_WORLD includes all processes.
+ *
+ * MPI_Comm_rank(
+ *     MPI_Comm communicator,
+ *     int* rank)
+ *
+ *   return the rank of a process in a communicator.
+ *   each process of a communicator is assigned a rank inscreasing from 0.
+ *
+ * MPI_Get_processor_name(
+ *     char* name,
+ *     int* name_length)
+ *
+ *   get processor name, hostname
+ *
+ * MPI_Finalize()
+ *   clean up the environment, no more MPI functions after that.
+ */
 int main(int argc, char** argv)
 {
     MPI_Init(NULL, NULL);
