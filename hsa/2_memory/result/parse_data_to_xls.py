b8deef6b00cc0ff8b5808563ca67d451c97c8f55
Junwei Zhang <zjunweihit@sina.com>
2019-07-05 00:00:08 +0800

    hsa: update tool to parse data to xls
    write data like in xls, need to verify with xls

--- hsa/2_memory/result/parse_data_to_xls.py
+++ hsa/2_memory/result/parse_data_to_xls.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-import xlsxwriter
+#import xlsxwriter
 import re
-#import sys
 
 def read_file(fname):
     d2d_list = []
@@ -37,7 +36,29 @@ def read_file(fname):
 #
 #wb.close()
 
+def collect_data(name, a, b, c, rowBase):
+    if len(a) != len(b):
+        print "Error: input arrays length are not equal!\n"
+
+    j = 1
+    colBase = 1
+    print ("(%d, %d): %s" % (rowBase,     colBase - 1, name))
+    print ("(%d, %d): %s" % (rowBase + 1, colBase - 1, "Shader DMA"))
+    print ("(%d, %d): %s" % (rowBase + 2, colBase - 1, "CPDMA"))
+    print ("(%d, %d): %s" % (rowBase + 3, colBase - 1, "SDMA"))
+
+    for i in range(len(a)):
+        print ("(%d, %d): %s" % (rowBase,     colBase + i, a[i][0]))
+        print ("(%d, %d): %s" % (rowBase + 1, colBase + i, a[i][1]))
+        print ("(%d, %d): %s" % (rowBase + 2, colBase + i, b[i][1]))
+        print ("(%d, %d): %s" % (rowBase + 3, colBase + i, c[i][1]))
+
 if __name__ == "__main__":
-    (d2d, h2d, d2h) = read_file("shader_dma.log")
-    #write_xlsx(engine, d2d, h2d, d2h, base_line)
+    (shdma_d2d, shdma_h2d, shdma_d2h) = read_file("shdma.log")
+    (cpdma_d2d, cpdma_h2d, cpdma_d2h) = read_file("cpdma.log")
+    (sdma_d2d,  sdma_h2d,  sdma_d2h)  = read_file("sdma.log")
+
+    collect_data("D2D", shdma_d2d, cpdma_d2d, sdma_d2d, 0)
+    collect_data("H2D", shdma_h2d, cpdma_h2d, sdma_h2d, 5)
+    collect_data("D2H", shdma_d2h, cpdma_d2h, sdma_d2h, 10)
 
