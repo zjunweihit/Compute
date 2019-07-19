d19888f9630aa1759b3f2680180d24bfbf752e4e
Junwei Zhang <zjunweihit@163.com>
2019-07-19 14:39:46 +0800

    hsa: update comments in vector_add.s

--- hsa/0_vector_add/vector_add.s
+++ hsa/0_vector_add/vector_add.s
@@ -23,30 +23,30 @@ vector_add:
     //   v0 - workitem id
 
     s_load_dwordx2  s[4:5], s[0:1], 0x10  // load out_ptr into s[4:5] from kernarg
-    s_load_dwordx4  s[0:3], s[0:1], 0x00  // load in_ptr into s[0:1] and index_ptr into s[2:3] from kernarg
+    s_load_dwordx4  s[0:3], s[0:1], 0x00  // load a into s[0:1] and b into s[2:3] from kernarg
     v_lshlrev_b32  v0, 2, v0              // v0 *= 4;
     s_waitcnt     lgkmcnt(0)              // wait for memory reads to finish
 
-    // compute address of corresponding element of index buffer
-    // i.e. v[1:2] = &index[workitem_id]
+    // compute address of corresponding element of out buffer
+    // i.e. v[1:2] = &b[workitem_id]
     v_add_co_u32     v1, vcc, s2, v0
     v_mov_b32     v2, s3
     v_addc_co_u32    v2, vcc, v2, 0, vcc
 
     // compute address of corresponding element of in buffer
-    // i.e. v[3:4] = &in[workitem_id]
+    // i.e. v[3:4] = &a[workitem_id]
     v_add_co_u32     v3, vcc, s0, v0
     v_mov_b32     v4, s1
     v_addc_co_u32    v4, vcc, v4, 0, vcc
 
-    flat_load_dword  v1, v[1:2] // load index[workitem_id] into v1
-    flat_load_dword  v2, v[3:4] // load in[workitem_id] into v2
+    flat_load_dword  v1, v[1:2] // load a[workitem_id] into v1
+    flat_load_dword  v2, v[3:4] // load b[workitem_id] into v2
     s_waitcnt     vmcnt(0) & lgkmcnt(0) // wait for memory reads to finish
 
     v_add_f32_e32     v1, v1, v2
 
     // compute address of corresponding element of out buffer
-    // i.e. v[3:4] = &out[workitem_id]
+    // i.e. v[3:4] = &c[workitem_id]
     v_add_co_u32     v3, vcc, s4, v0
     v_mov_b32     v2, s5
     v_addc_co_u32    v4, vcc, v2, 0, vcc
