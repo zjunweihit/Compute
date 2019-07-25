5ed6316978040d95891361a2736100bf5461d431
Junwei Zhang <zjunweihit@163.com>
2019-07-25 13:57:37 +0800

    tf: update comments to explain some details

--- tf/6_linear_regression/linear_regression.py
+++ tf/6_linear_regression/linear_regression.py
@@ -24,19 +24,24 @@ training_set = pd.read_csv("boston_train.csv", skipinitialspace=True, skiprows=1
 test_set = pd.read_csv("boston_test.csv", skipinitialspace=True, skiprows=1, names=COLUMNS)
 prediction_set = pd.read_csv("boston_predict.csv", skipinitialspace=True, skiprows=1, names=COLUMNS)
 
-# print(training_set.shape, test_set.shape, prediction_set.shape)
+# get features in a data frame
+test = pd.DataFrame({k: training_set[k].values for k in FEATURES})
+print(test)
 
+# feature column is used to transform raw data to estimator
 feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]
 print(feature_cols)
 
 estimator = tf.estimator.LinearRegressor(feature_cols, model_dir="train")
 
+# training data steps(1000) times
 estimator.train(input_fn=get_input_fn(training_set,
                                       num_epochs=None,
                                       n_batch=128,
                                       shuffle=False),
                 steps=1000)
 
+# evaluate the linearregression estimator with test data, getting the loss data
 ev=estimator.evaluate(
     input_fn=get_input_fn(test_set,
                           num_epochs=1,
@@ -45,10 +50,13 @@ ev=estimator.evaluate(
 )
 
 loss_score = ev["loss"]
+
+print(ev)
 print("Loss: {0:f}".format(loss_score))
 
 print(training_set['medv'].describe())
 
+# predict the 6 features
 y = estimator.predict(
     input_fn=get_input_fn(
         prediction_set,
